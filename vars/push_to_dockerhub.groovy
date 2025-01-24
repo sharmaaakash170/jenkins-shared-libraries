f0490ee9252e74486356090900be752bef88fa3b
Aakash Sharma <65845474+sharmaaakash170@users.noreply.github.com>
2025-01-24 22:17:42 +0530

    Update push_to_dockerhub.groovy

--- vars/push_to_dockerhub.groovy
+++ vars/push_to_dockerhub.groovy
@@ -1,9 +1,7 @@
 def call(String prevBuildAppName, String dockerCreds, String version){
-  sh "withCredentials([usernamePassword(credentialsId:${dockerCreds}, 
-                                        usernameVariable:"username", 
-                                        passwordVariable:"password")]){
-    sh "docker login -u ${env.username} -p ${env.password}"
-    sh "docker image tag ${preBuildAppName} ${env.username}/${preBuildAppName}:${version}"
-    sh "docker push ${env.username}/${preBuildAppName}:${version}"
+  withCredentials([usernamePassword(credentialsId:${dockerCreds}, usernameVariable:"username", passwordVariable:"password")]){
+      sh "docker login -u ${env.username} -p ${env.password}"
+      sh "docker image tag ${preBuildAppName} ${env.username}/${preBuildAppName}:${version}"
+      sh "docker push ${env.username}/${preBuildAppName}:${version}"
   }
 }
